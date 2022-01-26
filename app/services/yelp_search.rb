49b402423a23ea633d3c454c7d9e5f6132bf27f2
0lafe <0lafethehorsescrub@gmail.com>
2022-01-26 17:36:00 -0500

    added yelp search service

---
+++ app/services/yelp_search.rb
@@ -0,0 +1,17 @@
+class YelpSearch
+    
+    BASE_URL = "https://api.yelp.com/v3/"
+    API_KEY = ENV['API_KEY']
+
+    def self.search(params)
+        # return params
+        url = "#{BASE_URL}#{params["search_type"]}/search"
+        parameters = {
+            location: params["location"],
+            offset: 0
+        }
+
+        return JSON.parse(HTTP.auth("Bearer #{API_KEY}").get(url, params: parameters).body)
+    end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -15,22 +15,6 @@ ActiveRecord::Schema.define(version: 2022_01_26_204912) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "answers", force: :cascade do |t|
-    t.string "title", null: false
-    t.string "description", null: false
-    t.bigint "question_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["question_id"], name: "index_answers_on_question_id"
-  end
-
-  create_table "questions", force: :cascade do |t|
-    t.string "title", null: false
-    t.string "description", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
