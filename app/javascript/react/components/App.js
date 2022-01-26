3def0709f166db1078e22abb2a8a851f08b24ef5
Daniel <danielzhdanov31@gmail.com>
2022-01-26 16:41:11 -0500

    added devise

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navbar from "./NavBar";
 
 export const App = (props) => {
-  return (<h1>Make It So React</h1>)
-}
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<Switch>
+				<Route exact path='/'></Route>
+			</Switch>
+		</BrowserRouter>
+	);
+};
 
-export default App
+export default App;
---
+++ app/services/YelpSearch.rb
@@ -0,0 +1,7 @@
+class YelpSearch
+ base_url = "https://api.yelp.com/v3/businesses/search"
+ API_Key = ENV['API_KEY']
+
+ def self.search
+
+end
